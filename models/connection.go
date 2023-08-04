184c87e227fec1c44b1f30120725f3345e04b14f
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-08-04 18:30:20 -0300

    fix: serial read is working on windows right now

--- models/connection.go
+++ models/connection.go
@@ -1,7 +1,9 @@
 package models
 
 import (
-    "go.bug.st/serial"
+	"strings"
+
+	"go.bug.st/serial"
 )
 
 // Esse objeto deve ser **global**, ele vai guardar os métodos necessário para
@@ -10,17 +12,27 @@ var Connection serial.Port
 
 // Configurações básicas da conexão com placa.
 var ConnectionMode = &serial.Mode{
-    BaudRate: 9600,
+	BaudRate: 9600,
 }
 
 // Função que abstrai a leitura da placa, ela vai retornar a última coisa que
 // foi registrada na porta serial até o caractere nulo ('\0').
 func GetBoardOutput() (string, error) {
-    buff := make([]byte, 128)
-    rLen, err := Connection.Read(buff)
-    if err != nil {
-        return "", err
-    }
+	buff := make([]byte, 128)
+	result := ""
 
-    return string(buff[:rLen]), nil
+	for {
+		ilen, err := Connection.Read(buff)
+		if err != nil {
+			return "", err
+		}
+
+		result += string(buff[:ilen])
+
+		if strings.Contains((string(buff[:ilen])), "\n") {
+			break
+		}
+	}
+
+	return result, nil
 }
