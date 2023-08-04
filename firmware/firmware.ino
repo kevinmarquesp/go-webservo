27324b0d89713564af2b5b138bda881935a44f06
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-08-03 21:58:43 -0300

    feat(arduino): chuck that converts the string and writ the values on the servos

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,7 +1,12 @@
+#include <Servo.h>
+
 #define AMOUNT 3
 
+const uint8_t pinarr[AMOUNT] = {  7,   6,   5};
 const uint8_t minarr[AMOUNT] = {  0,  30,  10};
 const uint8_t maxarr[AMOUNT] = {180, 150,  90};
+
+Servo actor[AMOUNT];
 
 void setup(void)
 {
@@ -12,6 +17,9 @@ void setup(void)
     webconfig.concat(String(minarr[i]) + String("-"));
     webconfig.concat(i == AMOUNT - 1 ?
       String(maxarr[i]) : String(maxarr[i]) + String(","));
+
+    actor[i].attach(pinarr[i]);
+    actor[i].write(minarr[i]);
   }
 
   Serial.begin(9600);
@@ -20,4 +28,25 @@ void setup(void)
 
 void loop(void)
 {
+  if (not Serial.available())
+    return;
+
+  String rawData = Serial.readStringUntil('\n');
+  String strData[AMOUNT];
+  uint8_t j = 0, s = 0;
+
+  for (uint8_t i = 0; i < rawData.length(); ++i)
+  {
+    if (rawData.charAt(i) == ' ' or i == rawData.length() - 1)
+    {
+      strData[j] = rawData.substring(s, i + 1);
+      s = i;
+      j++;
+    }
+  }
+
+  for (uint8_t i = 0; i < AMOUNT; ++i)
+  {
+    actor[i].write(strData[i].toInt());
+  }
 }
