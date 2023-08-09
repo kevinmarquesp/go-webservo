22f7f98f2c6fd22fd3bc8c2443733f65b9355abb
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-08-09 18:26:54 -0300

    feat(arduino): parallel movements now starts and end at the same time

--- firmware/sketch/src/helpers/routines/routines.cpp
+++ firmware/sketch/src/helpers/routines/routines.cpp
@@ -20,21 +20,33 @@ void livemovimentRoutine(u8 pos, u8 deg, u16 vel)
 void parallelmovimentRoutine(u8* degarr, u16 vel, u8 buffsize)
 {
   String pastpos, nowpos;
-  u8 doneCounter = 0;
+  u8 bspos = 0, doneCounter = 0, lenarr[buffsize];
+  u16 total, velarr[buffsize];
+
+  // get the length of each movement
+  for (u8 i = 0; i < buffsize; ++i)
+    lenarr[i] = abs(Motor[i].read() - degarr[i]);
+
+  // select the slowest movement and calc the total
+  for (u8 i = 0; i < buffsize; ++i)
+    bspos = lenarr[i] > lenarr[bspos] ? i : bspos;
+
+  total = lenarr[bspos] * vel;
+
+  // use the total value to calc the movement for each servo
+  for (u8 i = 0; i < buffsize; ++i)
+    velarr[i] = total / lenarr[i];
 
   pastpos = getallServopos(Motor, buffsize);
 
+  // stop the program execution to move the servos
   while (doneCounter <= buffsize)
   {
     for (u8 i = 0; i < buffsize; ++i)
-    {
-      u8 deg = degarr[i];
+      Motor[i].move(Motor[i].is(0), degarr[i], velarr[i]);
 
-      Motor[i].move(Motor[i].is(0), deg, vel);
-
-      if (isAlldone(Motor, buffsize))
-        doneCounter++;
-    }
+    if (isAlldone(Motor, buffsize))
+      doneCounter++;
   }
 
   nowpos = getallServopos(Motor, buffsize);
